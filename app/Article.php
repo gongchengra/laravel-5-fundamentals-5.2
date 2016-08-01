637f5e8a821fdc0da37f7c012ffc59c49a7f7be3
龚成 <gongcheng749@pingan.com.cn>
2016-08-01 05:53:05 +0000

    add file in class 11

--- app/Article.php
+++ app/Article.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     protected $fillable = ['title', 'body', 'published_at'];
+
+    protected $dates = ['published_at'];
+
+    public function setPublishedAtAttribute($date)
+    {
+        //        $this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date);
+        $this->attributes['published_at'] = Carbon::parse($date);
+    }
+
+    public function scopePublished($query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+    public function scopeUnpublished($query)
+    {
+        $query->where('published_at', '>', Carbon::now());
+    }
 }
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -18,7 +18,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::latest('published_at')->get();
+        $articles = Article::latest('published_at')->published()->get();
+//        $articles = Article::latest('published_at')->unpublished()->get();
         return view('articles.index', compact('articles'));
     }
 
@@ -35,9 +36,7 @@ class ArticleController extends Controller
 
     public function store()
     {
-        $input = Request::all();
-        $input['published_at'] = Carbon::now();
-        Article::create($input);
+        Article::create(Request::all());
         return redirect('articles');
     }
 }
