1c19b178f666b17a8b0e888a5029de07b063d8aa
龚成 <gongcheng749@pingan.com.cn>
2016-08-15 03:59:09 +0000

    clean js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,19 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss').coffee('coffee.module');
-    //    mix.sass('app.scss', 'resources/css');
+    mix.sass('app.scss', 'resources/assets/css/app.css')
+    .coffee('coffee.module','resources/assets/js/coffee.js');
     mix.styles([
-        'vendor/bootstrap.min.css',
+        'bootstrap.min.css',
+        'select2.min.css',
         'app.css',
-        'vendor/select2.min.css',
-    ], 'public/output/final.css', 'public/css');
-    mix.version('public/output/final.css');
+    ]);
     mix.scripts([
         'jquery-1.12.4.min.js',
         'bootstrap.min.js',
         'select2.min.js',
-    ],'', 'public/js');
+        'coffee.js'
+    ]);
+    mix.version(['public/css/all.css', 'public/js/all.js']);
     //    mix.phpUnit();
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog</title>
-    <link href="{{ elixir('output/final.css') }}" rel="stylesheet">
+    <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         @include('flash::message')
         @yield('content')
     </div>
-    <script src="/js/all.js"></script>
+   	<script src="{{ elixir('js/all.js') }}"></script> 
     <script>
-//    $('div.alert').not('.alert-important').delay(3000).slideUp(300);
         $('#flash-overlay-modal').modal();
     </script>
     @yield('footer')
